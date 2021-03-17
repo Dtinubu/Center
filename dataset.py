ee5c525a78fa4cc5a9d48b9df04f0d78b718644b
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-17 11:43:12 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -47,9 +47,9 @@ def create_datasetsR(race, Number_of_pics, dataroot, train_val_split=0.9):
           def add_class(image):
             image_path = os.path.join(images_root, name, image)
             return (image_path, klass, name)
-            images_of_person = os.listdir(os.path.join(images_root, name))
-            total = len(images_of_person)
-            whole_set += map(
+          images_of_person = os.listdir(os.path.join(images_root, name))
+          total = len(images_of_person)
+          whole_set += map(
                     add_class,
                     images_of_person)
            
