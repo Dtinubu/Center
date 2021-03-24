65c5eedaffe58c044b5a8ffa5aabbb03474cd22f
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 10:19:19 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -62,11 +62,6 @@ def fold(folds, whole_set,):
             train_dataset = DatasetSplit(data, (i + 1) * split_length, tot_length - split_length)
             val_dataset = DatasetSplit(data, i * split_length, split_length)
             yield (val_dataset,train_dataset)
-         validation_set = val_dataset
-         training_set = train_dataset
-         return (validation_set, training_set)
-
-
 
 class DatasetSplit(data.Dataset):
     def __init__(self, dataset, index, length):
