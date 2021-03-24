4bfa308314a8219766f46ba4ed07d7d7f3be9a29
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 16:22:04 -0400

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -55,12 +55,12 @@ def create_datasetsR(race, Number_of_pics, dataroot, train_val_split=0.9):
            
     return whole_set, len(names)
 
-def fold(folds, whole_set):
-         tot_length = len(whole_set)
+def fold(folds, dataset):
+         tot_length = len(dataset)
          split_length = tot_length // folds
          for i in range(tot_length):
-            train_dataset = DatasetSplit(data, (i + 1) * split_length, tot_length - split_length)
-            val_dataset = DatasetSplit(data, i * split_length, split_length)
+            train_dataset = DatasetSplit(dataset, (i + 1) * split_length, tot_length - split_length)
+            val_dataset = DatasetSplit(dataset, i * split_length, split_length)
             yield (val_dataset,train_dataset)
 
 class DatasetSplit(data.Dataset):
