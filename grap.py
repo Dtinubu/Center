fef024c9a7320ef6733fb8a40cd8057dd933a6d2
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-05-19 15:46:52 -0400

    Update grap.py

--- grap.py
+++ grap.py
@@ -3,10 +3,10 @@ import os
 
 f = open( "learingrate_02.out.262859" , "r")
 
-train = re.search(\{train:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+))
+train = re.search( \[train:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+) )
 
-train_end = re.search(\[train:(\d+)\] finished. cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - together: (\d+\.\d+) - top1 acc: (\d+\.\d+) % - top3 acc: (\d+\.\d+))
+train_end = re.search( \[train:(\d+)\] finished. cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - together: (\d+\.\d+) - top1 acc: (\d+\.\d+) % - top3 acc: (\d+\.\d+) )
 
-validate =  re.search(\[validate:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+))
+validate =  re.search( \[validate:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+) )
 
 print( train.group() + train.group(1) + train.group(2) + train.group(3) )
