9d6653d64601be265c6e5ad8032c782656be0946
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-05-19 15:45:02 -0400

    Add files via upload

---
+++ grap.py
@@ -0,0 +1,14 @@
+import re
+import os
+
+Races =  ['Caucasian','Indian','Asian','African']
+
+	f = open( "learingrate_02.out.262859" , "r")
+
+train = re.search(\{train:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+))
+
+train_end = re.search(\[train:(\d+)\] finished. cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - together: (\d+\.\d+) - top1 acc: (\d+\.\d+) % - top3 acc: (\d+\.\d+))
+
+validate =  re.search(\[validate:(\d+)\] cross entropy loss: (\d+\.\d+) - center loss: (\d+\.\d+) - total weighted loss: (\d+\.\d+))
+
+print( train.group() + train.group(1) + train.group(2) + train.group(3) )
